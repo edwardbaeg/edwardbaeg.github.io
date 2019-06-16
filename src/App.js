16e3d0336f5f6974705570a88e3d57e2ac5c1e49
edwardbaeg <edwardbaeg9@gmail.com>
2019-06-15 18:27:42 -0700

    Test styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+const header = css`
+  font-size: 48px;
+`;
 
 export default function App(props) {
   return (
-    <div>
+    <div css={header}>
       This is edwardbaeg.com
     </div>
   );
